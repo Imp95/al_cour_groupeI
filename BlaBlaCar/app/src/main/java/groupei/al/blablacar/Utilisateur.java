eca7120d80316e69f8648e96c7d72ab2b42cb9b9
Ivan Picard Marchetto <ivan.picardmarchetto@gmail.com>
2018-10-17 17:23:29 +0200

    Android class

---
+++ BlaBlaCar/app/src/main/java/groupei/al/blablacar/Utilisateur.java
@@ -0,0 +1,69 @@
+package groupei.al.blablacar;
+
+import java.util.Date;
+
+public class Utilisateur {
+    private String Email;
+    private String Nom;
+    private String Prenom;
+    private Date Date_de_naissance;
+    private String Telephone;
+    private int Solde;
+
+    public Utilisateur(String email, String nom, String prenom, Date date_de_naissance, String telephone, int solde) {
+        Email = email;
+        Nom = nom;
+        Prenom = prenom;
+        Date_de_naissance = date_de_naissance;
+        Telephone = telephone;
+        Solde = solde;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
+    public String getPrenom() {
+        return Prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        Prenom = prenom;
+    }
+
+    public Date getDate_de_naissance() {
+        return Date_de_naissance;
+    }
+
+    public void setDate_de_naissance(Date date_de_naissance) {
+        Date_de_naissance = date_de_naissance;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        Telephone = telephone;
+    }
+
+    public int getSolde() {
+        return Solde;
+    }
+
+    public void setSolde(int solde) {
+        Solde = solde;
+    }
+}
