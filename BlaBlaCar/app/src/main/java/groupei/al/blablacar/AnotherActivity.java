317b9caaca68c8bc7007683ad12ed5f4edfbe3ff
Ivan Picard Marchetto <ivan.picardmarchetto@gmail.com>
2018-10-23 18:32:07 +0200

    Exemple activity

---
+++ BlaBlaCar/app/src/main/java/groupei/al/blablacar/AnotherActivity.java
@@ -0,0 +1,35 @@
+package groupei.al.blablacar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { //Methode appeler a la création du l'app
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+        TextView textView = (TextView) findViewById(R.id.text);
+        Intent intent = this.getIntent();
+        int x = intent.getIntExtra("key",0);
+        textView.setText("x = "+x);
+
+    }
+
+    @Override
+    protected void onPause(){ //Methode appeler lorsque qu'on quitte l'app sans la refermer
+        super.onPause();
+
+    }
+    @Override
+    protected void onResume(){ //Methode appeler lorsque qu'on reviens sur l'app, est inclus dans le onCreate
+        super.onResume();
+    }
+
+
+
+}
--- BlaBlaCar/app/src/main/java/groupei/al/blablacar/MainActivity.java
+++ BlaBlaCar/app/src/main/java/groupei/al/blablacar/MainActivity.java
@@ -1,13 +1,44 @@
 package groupei.al.blablacar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //Methode appeler a la création du l'app
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);//Récuperer un buttong
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changerActivity(view);
+            }
+        });
+        // this.finish(); Comment fermé l'app
     }
+
+    @Override
+    protected void onPause(){ //Methode appeler lorsque qu'on quitte l'app sans la refermer
+        super.onPause();
+
+    }
+    @Override
+    protected void onResume(){ //Methode appeler lorsque qu'on reviens sur l'app, est inclus dans le onCreate
+        super.onResume();
+    }
+
+    public void changerActivity(View view) {
+        Intent intent = new Intent(this, AnotherActivity.class);
+        int x =10;
+        intent.putExtra("key", x); //faire passer des parametre
+        startActivity(intent);
+    }
+
+
 }
