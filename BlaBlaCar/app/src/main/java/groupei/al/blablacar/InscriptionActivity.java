303df7b669194a0b63762de6976ec7abcf765a73
Duminy Gaétan <gaetan.duminy@gmail.com>
2018-11-02 11:45:35 +0100

    Mock inscription / connexion

--- BlaBlaCar/app/src/main/java/groupei/al/blablacar/InscriptionActivity.java
+++ BlaBlaCar/app/src/main/java/groupei/al/blablacar/InscriptionActivity.java
@@ -1,10 +1,16 @@
-package groupei.al.blablacar;
+package groupei.al.blablacar.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+
+import groupei.al.blablacar.R;
+import groupei.al.blablacar.Tools.Checker;
 
 public class InscriptionActivity extends AppCompatActivity {
 
@@ -12,7 +18,6 @@ public class InscriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) { //Methode appeler a la création du l'app
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
-
         Button confirmer = (Button) findViewById(R.id.confirmer);
         confirmer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,7 +38,26 @@ public class InscriptionActivity extends AppCompatActivity {
     }
 
     public void changerActivityAcceuil(View view) {
-        Intent intent = new Intent(this, AcceuilActivity.class);
-        startActivity(intent);
+        if (mock_inscription()) {
+            Intent intent = new Intent(this, AcceuilActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private boolean mock_inscription() {
+        Checker.getInstance();
+        EditText mail = (EditText) findViewById(R.id.email);
+        EditText mdp = (EditText) findViewById(R.id.mdp);
+        EditText mdp2 = (EditText) findViewById(R.id.mdp2);
+        EditText nom = (EditText) findViewById(R.id.nom);
+        EditText prenom = (EditText) findViewById(R.id.prenom);
+        EditText naissance = (EditText) findViewById(R.id.naissance);
+        EditText tel = (EditText) findViewById(R.id.tel);
+        try {
+            return Checker.inscriptionIsValid(mail.getText().toString(), mdp.getText().toString(), mdp2.getText().toString(), naissance.getText().toString(), tel.getText().toString(), nom.getText().toString(), prenom.getText().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
--- BlaBlaCar/app/src/main/java/groupei/al/blablacar/MainActivity.java
+++ BlaBlaCar/app/src/main/java/groupei/al/blablacar/MainActivity.java
@@ -1,30 +1,56 @@
-package groupei.al.blablacar;
+package groupei.al.blablacar.Activities;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import groupei.al.blablacar.Tools.Checker;
+import groupei.al.blablacar.Tools.JSONSerializer;
+import groupei.al.blablacar.Tools.LoginHandler;
+import groupei.al.blablacar.Tools.LoginHandlerMock;
+import groupei.al.blablacar.Tools.LoginToken;
+import groupei.al.blablacar.R;
+import groupei.al.blablacar.Tools.RequestHandler;
 
 public class MainActivity extends AppCompatActivity {
 
     LoginHandler loginHandler;
     LoginToken token;
     TextView error;
+    RequestHandler requestHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //Methode appeler a la création du l'app
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loginHandler = new LoginHandlerMock();
+        //requestHandler = RequestHandler.getInstance(getApplicationContext());
+        JSONSerializer.getInstance();
         error = (TextView) findViewById(R.id.errorText);
         error.setTextColor(Color.rgb(255,0,0));
-
-
 
         Button connexion = (Button) findViewById(R.id.connexion);//Récuperer un button
         connexion.setOnClickListener(new View.OnClickListener() {
@@ -71,6 +97,38 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);*/
         EditText mail = (EditText) findViewById(R.id.email);
         EditText mdp = (EditText) findViewById(R.id.mdp);
+        Checker.getInstance();
+        /*JSONObject js = JSONSerializer.getConnexionJSON(mail.toString(), mdp.toString());
+        String url ="http://192.168.0.1:4000/receive_event";
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.POST,url,js,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("connexion", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("connexion", "Error: " + error.getMessage());
+            }}) {
+
+             // Passing some request headers
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "/application/json; charset=utf-8");
+                return headers;
+            }
+        };
+
+        requestHandler.addToRequestQueue(jsonObjReq);
+        */
+
+
+        /*
         token = loginHandler.login(mail.getText().toString(),mdp.getText().toString());
         if(token==null){
             error.setText("Invalid login or password");
@@ -81,8 +139,15 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor mEditor = mPrefs.edit();
         mEditor.putString("TokenMail",token.getEmail());
         mEditor.putLong("TokenTime",token.getExpire());
-        Intent intent = new Intent(this, AcceuilActivity.class);
-        startActivity(intent);
+        */
+        try {
+            if (Checker.connexionIsValid(mail.getText().toString(), mdp.getText().toString())) {
+                Intent intent = new Intent(this, AcceuilActivity.class);
+                startActivity(intent);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void changerActivityInscription(View view) {
