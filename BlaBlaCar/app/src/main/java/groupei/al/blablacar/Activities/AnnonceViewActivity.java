8302895b124df569fba52c699d63221ba1c33b79
Ivan Picard Marchetto <ivan.picardmarchetto@gmail.com>
2018-11-07 20:27:59 +0100

    Annonce adapter

---
+++ BlaBlaCar/app/src/main/java/groupei/al/blablacar/Activities/AnnonceViewActivity.java
@@ -0,0 +1,6 @@
+package groupei.al.blablacar.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class AnnonceViewActivity  extends AppCompatActivity {
+}
---
+++ BlaBlaCar/app/src/main/java/groupei/al/blablacar/Tools/AnnonceAdapter.java
@@ -0,0 +1,72 @@
+package groupei.al.blablacar.Tools;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import groupei.al.blablacar.Activities.AnnonceViewActivity;
+import groupei.al.blablacar.Activities.OffreViewActivity;
+import groupei.al.blablacar.Entities.Annonce;
+import groupei.al.blablacar.R;
+
+public class AnnonceAdapter extends RecyclerView.Adapter<AnnonceAdapter.MyViewHolder> {
+    private List<Annonce> mDataset;
+    private Activity parent;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public MyViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public AnnonceAdapter(List<Annonce> myDataset, Activity activity) {
+        mDataset = myDataset;
+        parent=activity;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public AnnonceAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                          int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder,final int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset.get(position).toString());
+        holder.mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(parent,AnnonceViewActivity.class);
+                intent.putExtra("annonce",mDataset.get(position));
+            }
+        });
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
