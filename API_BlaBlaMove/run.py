26e539188e6c9ec4e6230d196172678c7f4fc7a0
Dylan Ritrovato <dylan.ritrovato@outlook.com>
2019-01-11 08:58:40 +0100

    renommage ancienne api, possibilité de choisir host + port au lancement sur nouvelle api

--- API_BlaBlaMove/run.py
+++ API_BlaBlaMove/run.py
@@ -1,5 +1,20 @@
 import api
+import sys
 from api import app
 
 if __name__ == "__main__":
-    app.run(host='192.168.0.42',port='80',debug=True)
+    host = 'localhost'
+    port = '5555'
+
+    argv = sys.argv[1:]
+
+    for i in range(0, len(argv)):
+        splitted = argv[i].split('=', 1)
+        if splitted[0] == 'host':
+            host = splitted[1]
+            continue
+        elif splitted[0] == 'port':
+            port = splitted[1]
+            continue
+
+    app.run(host=host,port=port,debug=True)
