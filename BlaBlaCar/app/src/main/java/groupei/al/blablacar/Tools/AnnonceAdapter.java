fbe16cba3b552fc30a99541850547360b9b4a7b3
Ivan Picard Marchetto <ivan.picardmarchetto@gmail.com>
2019-01-21 16:35:00 +0100

    annonce view

--- BlaBlaCar/app/src/main/java/groupei/al/blablacar/Tools/AnnonceAdapter.java
+++ BlaBlaCar/app/src/main/java/groupei/al/blablacar/Tools/AnnonceAdapter.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.LinkedList;
@@ -27,10 +29,22 @@ public class AnnonceAdapter extends RecyclerView.Adapter<AnnonceAdapter.MyViewHo
     // you provide access to all the views for a data item in a view holder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
-        public MyViewHolder(TextView v) {
+        public RelativeLayout mView;
+        public TextView departTextField,ariveeTextField,payTextField,bagageTextField,dateTextField;
+        public Button addToPanierButton;
+        public MyViewHolder(RelativeLayout v) {
             super(v);
-            mTextView = v;
+            mView = v;
+            init();
+        }
+
+        private void init() {
+            departTextField = (TextView) mView.findViewById(R.id.departTextField);
+            ariveeTextField = (TextView) mView.findViewById(R.id.ariveeTextField);
+            payTextField = (TextView) mView.findViewById(R.id.payTextField);
+            bagageTextField = (TextView) mView.findViewById(R.id.bagageTextField);
+            dateTextField = (TextView) mView.findViewById(R.id.dateTextField);
+            addToPanierButton = (Button) mView.findViewById(R.id.addToPanierButton);
         }
     }
 
@@ -46,23 +60,33 @@ public class AnnonceAdapter extends RecyclerView.Adapter<AnnonceAdapter.MyViewHo
     public AnnonceAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                           int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_text_view, parent, false);
+        RelativeLayout v = (RelativeLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.annonce_view, parent, false);
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(MyViewHolder holder,final int position) {
+    public void onBindViewHolder(final MyViewHolder holder,final int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataset.get(position).toString());
-        holder.mTextView.setOnClickListener(new View.OnClickListener() {
+        Annonce annonce = mDataset.get(position);
+        holder.departTextField.setText("Depart : " + annonce.getAdresse_depart());
+        holder.ariveeTextField.setText("Arrivee : " + annonce.getAdresse_arrivee());
+        holder.payTextField.setText("Paiment : " + annonce.getPaiement());
+        holder.bagageTextField.setText("Bagage : " + annonce.getBagage());
+        holder.dateTextField.setText("Date : " + annonce.getDate_Debut());
+        holder.addToPanierButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (! panier.contains(mDataset.get(position))) {
                     panier.add(mDataset.get(position));
+                    holder.addToPanierButton.setText("-");
+                    parent.getCart().setText("" + panier.size());
+                }else {
+                    panier.remove(mDataset.get(position));
+                    holder.addToPanierButton.setText("+");
                     parent.getCart().setText("" + panier.size());
                 }
             }
