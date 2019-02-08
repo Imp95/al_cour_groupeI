302ddc8bd8cd5d85751b43763e989d34d2f81354
Duminy Gaétan <gaetan.duminy@gmail.com>
2019-02-08 09:33:41 +0100

    #8 Force les champs d'inscription

--- API_Pro_web/js/inscription.js
+++ API_Pro_web/js/inscription.js
@@ -15,30 +15,36 @@ function inscription() {
     var mdp2 = document.getElementById("mdp2").value;
     var societe = document.getElementById("societe").value;
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (mdp == mdp2) {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var json = JSON.parse(xhr.responseText);
 
-                if (json.status) {
-                    console.log(json.body.firstname + " " + json.body.name);
-                    // CHANGER DE PAGE
-                    localStorage.setItem("email", json.body.email);
-                    localStorage.setItem("firstname", json.body.firstname);
-                    localStorage.setItem("amount", json.body.amount);
-                    document.location.href = "./acceuil.html";
-                } else {
-                    alert("Erreur: " + json.body);
+    if (mdp == mdp2) {
+        if (email != "" & lastname != "" & firstname != "" & phone != "" & societe != "") {
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var json = JSON.parse(xhr.responseText);
+
+                    if (json.status) {
+                        console.log(json.body.firstname + " " + json.body.name);
+                        // CHANGER DE PAGE
+                        localStorage.setItem("email", json.body.email);
+                        localStorage.setItem("firstname", json.body.firstname);
+                        localStorage.setItem("amount", json.body.amount);
+                        document.location.href = "./acceuil.html";
+                    } else {
+                        alert("Erreur: " + json.body);
+                    }
                 }
-            }
+            };
         }
         else {
-            alert("Erreur: Les mots de passe sont différents");
+            alert("Erreur: Un ou plusieurs champs manquants");
         }
-    };
+    }
+    else {
+        alert("Erreur: Les mots de passe sont différents");
+    }
     var data =
         '{' +
         '"action":"Inscription",' +
